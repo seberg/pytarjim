18013059443e55b9c4bf33f6cc7e429050269799
Sebastian Berg <sebastian@sipsolutions.net>
2011-12-02 22:39:45 +0100

    Initial working commit.

---
+++ pytarjim.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+__author__ = 'Sebastian Berg'
+__license__ = 'LGPL v3'
+
+import sys; sys.argv[0] = 'PyTarjim'
+import os
+import gtk, gobject
+import pango
+
+import tecpy
+
+translits = [('Vocalize (No Alif)', 'map/mappings/arabtex-fdf2noalif-voc.map'.replace('/', os.path.sep)),
+    ('Fullvocalize (No Alif)', 'map/mappings/arabtex-fdf2noalif-fullvoc.map'.replace('/', os.path.sep)),
+    ('No vocalization (No Alif)', 'map/mappings/arabtex-fdf2noalif-novoc.map'.replace('/', os.path.sep)),
+    ('Vocalize (Alif)', 'map/mappings/arabtex-fdf2alif-voc.map'.replace('/', os.path.sep)),
+    ('Fullvocalize (Alif)', 'map/mappings/arabtex-fdf2alif-fullvoc.map'.replace('/', os.path.sep)),
+    ('No vocalization (Alif)', 'map/mappings/arabtex-fdf2alif-novoc.map'.replace('/', os.path.sep)),
+    ('DMG Transliteration', 'map/mappings/arabtex-trans-dmg.map'.replace('/', os.path.sep)),
+    ('Transliteration', 'map/mappings/arabtex-trans-loc.map'.replace('/', os.path.sep))]
+
+translit_dict = dict(translits)
+config_file = 'pyjarjim_config.txt'
+
+class Transliterator:
+    def on_window_destroy(self, widget, data=None):
+        pos = self.last_position
+        size = self.last_size
+        f = file(config_file, 'w')
+        f.write('%i %i %i %i\n' % (pos[0], pos[1], size[0], size[1]))
+        f.write(self.font_button.get_font_name())
+        f.write('\n%s' % self.translitpicker.get_active())
+        f.close()
+        gtk.main_quit()
+    
+
+    def __init__(self):
+        # use GtkBuilder to build our interface from the XML file 
+        builder = gtk.Builder()
+        builder.add_from_file("window.glade") 
+    
+        self.changed = False
+        self.window = builder.get_object("window")
+        
+        self.text_trans = builder.get_object("trans")
+        self.text_arab = builder.get_object("arabic")
+        
+        self.window.set_keep_above(True)
+        self.trans_buffer = self.text_trans.get_buffer()
+        self.arab_buffer = self.text_arab.get_buffer() 
+        
+        # connect signals
+        builder.connect_signals(self)
+        self.trans_buffer.connect('changed', self.idle_edited)
+        
+        self.font_button = builder.get_object('arabfontbutton')
+        self.text_arab.modify_font(pango.FontDescription(self.font_button.get_font_name()))
+        
+        self.translitpicker = builder.get_object("translitpicker")
+        self.translit_list_store = gtk.ListStore(gobject.TYPE_STRING)
+        for n, _ in translits:
+            self.translit_list_store.append([n])
+        self.translitpicker.set_model(self.translit_list_store)
+        cell = gtk.CellRendererText()
+        self.translitpicker.pack_start(cell, True)
+        self.translitpicker.add_attribute(cell, "text", 0)
+        self.translitpicker.set_active(3)
+        
+        self.clipboard = gtk.Clipboard()
+        
+        try:
+            f = file(config_file)
+            l = f.read().split('\n')
+            p = l[0].split()
+            p = [int(_) for _ in p]
+            self.window.move(*p[:2])
+            self.window.resize(*p[2:4])
+            
+            self.font_button.set_font_name(l[1])
+            self.translitpicker.set_active(int(l[2])) 
+        except IOError:
+            pass
+        except:
+            print 'Invalid Config file!'
+
+        self.icon = gtk.StatusIcon()
+        self.icon.set_from_file('icon.svg')
+        self.icon.connect("activate", self.toggle_visibility)
+        
+        # just to make sure its not bogus
+        self.last_position = self.window.get_position()
+        self.last_size = self.window.get_size()
+        
+        self.last_position = self.window.get_position()
+
+    
+    def right_click_event(self, icon, button, time):
+        self.menu = gtk.Menu()
+
+        quit = gtk.MenuItem()
+        quit.set_label("Quit")
+
+        quit.connect("activate", self.close_from_tray)
+
+        self.menu.append(quit)
+        self.menu.show_all()
+    
+    
+    def toggle_visibility(self, *args, **kwargs):
+        visible = self.window.get_property("visible")
+        
+        if visible:
+            self.last_position = self.window.get_position()
+            self.last_size = self.window.get_size()
+            self.window.hide()
+        else:
+            self.window.show()
+            self.window.move(self.last_position[0], self.last_position[1])
+        
+    
+    def font_changed(self, font_button):
+        self.text_arab.modify_font(pango.FontDescription(font_button.get_font_name()))
+    
+    
+    def edited(self, button=None):
+        if not self.changed:
+            return
+        
+        buf = self.trans_buffer
+        text = buf.get_text(buf.get_start_iter(), buf.get_end_iter())
+        
+        arabic = self.transliterate(text)
+        
+        self.arab_buffer.set_text(arabic)
+        self.changed = False
+    
+    
+    def copy_arab(self, button):
+        buf = self.arab_buffer
+        text = buf.get_text(buf.get_start_iter(), buf.get_end_iter())
+        self.clipboard.set_text(text)
+
+    
+    def clear(self, button):
+        self.trans_buffer.set_text(u'')
+        self.arab_buffer.set_text(u'')
+    
+    
+    def idle_edited(self, text):
+        self.changed = True
+        
+        gobject.idle_add(self.edited)
+    
+    
+    def set_transliteration(self, picker):
+        """Set transliteration Mapping.
+        """
+        #print picker.get_active()
+        #print translits[picker.get_active()][1]
+        self.transliterate = tecpy.Mapping(translits[picker.get_active()][1])
+        self.changed = True
+        
+        gobject.idle_add(self.edited)
+    
+    def main(self):
+        self.window.show()
+        gtk.main()
+
+if __name__ == "__main__":
+    transliterator = Transliterator()
+    transliterator.main()
---
+++ tecpy.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+__author__ = 'Sebastian Berg'
+__license__ = 'LGPL v3'
+
+import re
+
+class _Control(unicode):
+    pass
+class _Group(unicode):
+    pass
+
+
+class Mapping(object):
+    """Class to read a very simple .map files and create a python mapping for it.
+    map file means a teckit .map function for parsing into .tec files...
+    
+    NOTES:  o Only forward direction is supported...
+            o reads only line wise...
+            o This is extremely limited...
+            o Uses python regexes, and thus is probably slow as hell compared
+                to real TecKit.
+    """
+    
+    def __init__(self, file):
+        """Given a .map file or string to it.
+        """
+        self.line_regexes = {re.compile(r'LHSName\s*"(?P<name>.*)"') : self.set_name,
+            re.compile(r'^RHSName\s*"(?P<name>.*)"$') : None,
+            re.compile(r'^Define\s+(?P<var>[A-z]+)\s+(?P<chars>.*)$') : self.set_def,
+            re.compile(r'^pass\(Unicode\)$') : self.new_pass,
+            re.compile(r'^(?P<ls>[^<>]+)(<>|>)(?P<rs>[^<>]*)$') : self.add_sub,
+            re.compile(r'^Class\s*(?P<class>\[[A-z]+\])\s*=\s*\((?P<group>.*)\)$') : self.add_group,
+        }
+        self.definitions = {}
+        self.passes = []
+        
+        if type(file) is str:
+            file = open(file, 'r')
+        
+        for line in file.readlines():
+            line = line.split(';')[0].strip() # remove comments, and whitespaces.
+            if line == '':
+                continue
+            
+            for r, f in self.line_regexes.iteritems():
+                match = r.match(line)
+                if match:
+                    if f is not None:
+                        f(match)
+                    break
+            else:
+                print 'Unmatched line:'
+                print '   ', repr(line)
+        
+        
+    def set_name(self, match):
+        self.name = match.groupdict()['name']
+    
+    
+    def get_unicode_string(self, string):
+        """Returns list of characters groups and everything.
+        """
+        s = re.findall('([()#<> |?\^]|@?=?[^()#<>= |?]*)', string)
+        s = [c.strip() for c in s if c.strip() != '']
+        res = []
+        for c in s:
+            if c.startswith('U+'):
+                c = c.split('..')
+                if len(c) == 1:
+                    res.append((unichr(int(c[0][2:], 16))))
+                else:
+                    # brutal, as regex really could handle this too...
+                    for i in xrange(int(c[0][2:], 16), int(c[1][2:], 16)+1):
+                        res.append((unichr(i)))
+            else:
+                try:
+                    c = self.definitions[c]
+                    res.extend(c)
+                except KeyError:
+                    # this must be some control character:
+                    res.append(_Control(c))
+        
+        return res
+    
+    
+    def set_def(self, match):
+        d = match.groupdict()
+        new_def = self.get_unicode_string(d['chars'])
+        
+        self.definitions[d['var']] = new_def
+        
+    
+    def add_group(self, match):
+        d = match.groupdict()
+        new_def = self.get_unicode_string(d['group'])
+        
+        if any([isinstance(c, _Control) for c in new_def]):
+            raise NotImplementedError('A definition should not include control characters.')
+        if any([isinstance(c, _Group) for c in new_def]):
+            raise NotImplementedError('A definition should not include grouped characters/classes.')
+        
+        self.definitions[d['class']] = [_Group(u''.join(new_def))]
+
+    def make_regex(self, commlist):
+        string = []
+        negate = False
+        
+        for m in commlist:
+            if isinstance(m, _Control):
+                if m == '^':
+                    negate = True
+                elif m == '#':
+                    string.append(u'(^|\s|$)')
+                elif m.startswith('='):
+                    # OK, ugly hack:
+                    if string[-1] == '?':
+                        del string[-1]
+                        string[-1] = u'(?P<%s>%s?)' % (m[1:], string[-1])
+                    else:
+                        string[-1] = u'(?P<%s>%s)' % (m[1:], string[-1])
+                else:
+                    string.append(m)
+            else:
+                if negate:
+                    string.append(u'(?!(%s))' % u'|'.join([re.escape(_) for _ in m]))
+                    negate = False
+                else:
+                    string.append(u'(%s)' % u'|'.join([re.escape(_) for _ in m]))
+        
+        string = u''.join(string)
+        return string
+        
+    
+    def add_sub(self, match):
+        d = match.groupdict()
+        
+        new = self.get_unicode_string(d['rs'].strip())
+        
+        if len(new) == 1:
+            # This is a special case of group assignment...
+            old = self.get_unicode_string(d['ls'].strip())
+            if len(old) == 1:  
+                new = new[0]
+                old = old[0]
+                if len(new) == len(old):
+                    for n, o in zip(new, old):
+                        n, o = n, re.compile(re.escape(o))
+                        def add(o, n):
+                            self.passes[-1].append([lambda x: o.sub(n, x), o, n])
+                        add(o, n)
+                    return
+                elif len(new) != 1:
+                    raise NotImplementedError('For list of characters, length must equal')
+        
+        _new = new
+
+        new = []
+        for m in _new:
+            if m.startswith('@'):
+                new.append(u'%%(%s)s' % m[1:])
+            else:
+                new.append(m)
+        new = u''.join(new)
+        
+        ls = d['ls'].strip().split('/')
+        
+        # We build ourself a mean regex now...
+        middle = self.make_regex(self.get_unicode_string(ls[0]))
+        
+        if len(ls) > 1:
+            l, r = ls[1].split('_')
+            l = l.strip()
+            if l == '':
+                before = u''
+            else:
+                before = u'(?P<__before_>%s)' % self.make_regex(self.get_unicode_string(l))
+                new = u'%(__before_)s' + new
+            r = r.strip()
+            if r == '':
+                after = u''
+            else:
+                after = u'(?P<__after_>%s)' % self.make_regex(self.get_unicode_string(r))
+                new = new + u'%(__after_)s'
+            
+            regex = re.compile(before + middle + after)
+            self.passes[-1].append([lambda x: regex.sub(lambda m: new % m.groupdict(), x), before + middle + after, new])
+        else:
+            regex = re.compile(middle)
+            self.passes[-1].append([lambda x: regex.sub(lambda m: new % m.groupdict(), x), middle, new])
+    
+    
+    def new_pass(self, match):
+        """This is actually not supposed to be used like this it seems. But
+        in any case, it does not matter much...
+        """
+        self.passes.append([])
+        
+
+
+    def __call__(self, string):
+        string = unicode(string)
+        for p in self.passes:
+            for sub in p:
+                #m = re.match(sub[1], string)
+                string = sub[0](string)
+
+        return string
